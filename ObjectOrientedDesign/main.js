94b0b1524e7a0d2c17a44a4145c609bf06f249e5
EliWoodard <elijahwoodard136@gmail.com>
2023-11-23 21:45:34 -0700

    Added JS Inheritance Example

--- ObjectOrientedDesign/main.js
+++ ObjectOrientedDesign/main.js
@@ -26,3 +26,63 @@ function BankAccount (accountNumber, accountHolderName, balance) {
  myBankAccount.deposit(1000);
  myBankAccount.withdraw(500);
  myBankAccount.showAccountDetails();
+
+ // Inheritance
+ // Base class for all user types
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    login() {
+        console.log(`${this.name} has logged in.`);
+    }
+
+    logout() {
+        console.log(`${this.name} has logged out.`);
+    }
+}
+
+// Admin user with additional privileges
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+
+    deleteUser(user) {
+        console.log(`${this.name} deleted the user ${user.name}.`);
+    }
+}
+
+// Moderator user with some moderation capabilities
+class Moderator extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+
+    moderateContent() {
+        console.log(`${this.name} is moderating content.`);
+    }
+}
+
+// Regular user with basic features
+class RegularUser extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+
+    postComment(comment) {
+        console.log(`${this.name} posts a comment: "${comment}"`);
+    }
+}
+
+// Usage
+let admin = new Admin('Alice', 'alice@example.com');
+let moderator = new Moderator('Bob', 'bob@example.com');
+let regularUser = new RegularUser('Charlie', 'charlie@example.com');
+
+admin.login();
+admin.deleteUser(regularUser);
+moderator.moderateContent();
+regularUser.postComment('Hello world!');
